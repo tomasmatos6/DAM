4c456e532af7d7b2652126c5e645634f1d6b84cd
Tomás Matos <tomasmatos16@gmail.com>
2024-05-07 17:47:05 +0100

    Final Project created

--- FinalProject/app/build.gradle.kts
+++ FinalProject/app/build.gradle.kts
@@ -1,16 +1,14 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
-    id("org.jetbrains.kotlin.kapt")
-    id("kotlin-parcelize")
 }
 
 android {
-    namespace = "dam_48286.pokedex"
+    namespace = "dam_488286.finalproject"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "dam_48286.pokedex"
+        applicationId = "dam_488286.finalproject"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
@@ -35,10 +33,6 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    buildFeatures {
-        viewBinding = true
-        dataBinding = true
-    }
 }
 
 dependencies {
@@ -48,17 +42,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.palette.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation ("androidx.activity:activity-ktx:1.9.0")
-    implementation ("androidx.fragment:fragment-ktx:1.7.0")
-    implementation("com.github.bumptech.glide:glide:4.16.0")
-    implementation("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation("com.squareup.retrofit2:converter-moshi:2.11.0")
-    implementation("com.squareup.moshi:moshi:1.15.1")
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
-    kapt("com.squareup.moshi:moshi-kotlin-codegen:1.15.1")
 }
